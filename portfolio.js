e1d77a7f3123332bc4c577ea2518e61df4a40c68
gealsanchez <94255143+gealsanchez@users.noreply.github.com>
2022-10-17 21:00:11 -0500

    Update portfolio.js
    6:1   error  Expected indentation of 2 spaces but found 4  indent
      10:1   error  Expected indentation of 2 spaces but found 4  indent
      13:39  error  Strings must use singlequote                  quotes
      14:15  error  Strings must use singlequote                  quotes
      15:39  error  Strings must use singlequote                  quotes
      16:15  error  Strings must use singlequote                  quotes
      17:39  error  Strings must use singlequote                  quotes
      18:15  error  Strings must use singlequote                  quotes
      19:39  error  Strings must use singlequote                  quotes
      20:40  error  Strings must use singlequote                  quotes
      21:40  error  Strings must use singlequote                  quotes
      22:40  error  Strings must use singlequote                  quotes
      23:40  error  Strings must use singlequote                  quotes
      24:21  error  Strings must use singlequote                  quotes
      25:20  error  Strings must use singlequote                  quotes
      26:20  error  Strings must use singlequote                  quotes
      27:20  error  Strings must use singlequote                  quotes
      28:20  error  Strings must use singlequote                  quotes
      29:21  error  Strings must use singlequote                  quotes
      30:21  error  Strings must use singlequote                  quotes
      31:21  error  Strings must use singlequote                  quotes
      33:22  error  Strings must use singlequote                  quotes
      34:22  error  Strings must use singlequote                  quotes
      35:22  error  Strings must use singlequote                  quotes
      47:1   error  Expected indentation of 2 spaces but found 4  indent
      51:1   error  Expected indentation of 2 spaces but found 4  indent
      55:1   error  Expected indentation of 2 spaces but found 4  indent
    
    ✖ 27 problems (27 errors, 0 warnings)
      27 errors and 0 warnings potentially fixable with the `--fix` option.

--- portfolio.js
+++ portfolio.js
@@ -3,36 +3,36 @@ const overlay = document.querySelector('.open');
 const cancel = document.querySelector('.close');
 
 hamburger.addEventListener('click', () => {
-    overlay.style.display = 'block';
+  overlay.style.display = 'block';
 });
 
 cancel.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
-const dynaaa = document.createElement("a");
-dynaaa.href = "index.html#works";
-const dynaab = document.createElement("a");
-dynaab.href = "index.html#about";
-const dynaac = document.createElement("a");
-dynaac.href = "index.html#contact";
-const dynahr = document.createElement("hr");
-const dynahr1 = document.createElement("hr");
-const dynahr2 = document.createElement("hr");
-const dynadiv = document.createElement("footer");
-const dynahr3 = document.createElement("hr");
-dynadiv.className = "dynadiv";
-dynaaa.className = "dynaaa";
-dynaab.className = "dynaab";
-dynaac.className = "dynaab";
-dynahr.className = "rule";
-dynahr1.className = "rule";
-dynahr2.className = "rule-8";
-dynahr3.className = "rule-7";
+const dynaaa = document.createElement('a');
+dynaaa.href = 'index.html#works';
+const dynaab = document.createElement('a');
+dynaab.href = 'index.html#about';
+const dynaac = document.createElement('a');
+dynaac.href = 'index.html#contact';
+const dynahr = document.createElement('hr');
+const dynahr1 = document.createElement('hr');
+const dynahr2 = document.createElement('hr');
+const dynadiv = document.createElement('footer');
+const dynahr3 = document.createElement('hr');
+dynadiv.className = 'dynadiv';
+dynaaa.className = 'dynaaa';
+dynaab.className = 'dynaab';
+dynaac.className = 'dynaab';
+dynahr.className = 'rule';
+dynahr1.className = 'rule';
+dynahr2.className = 'rule-8';
+dynahr3.className = 'rule-7';
 
-dynaaa.textContent = "Portfolio";
-dynaab.textContent = "About";
-dynaac.textContent = "Contact";
+dynaaa.textContent = 'Portfolio';
+dynaab.textContent = 'About';
+dynaac.textContent = 'Contact';
 
 overlay.appendChild(dynaaa);
 overlay.appendChild(dynahr);
@@ -44,13 +44,13 @@ overlay.appendChild(dynadiv);
 overlay.appendChild(dynahr3);
 
 dynaaa.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
 dynaab.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
 dynaac.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
